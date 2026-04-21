12088510681edc7a0a2f43b42d002cffc991002a
XiaoxingCHN <cassell1901@163.com>
2026-04-21 19:41:36 +0800

    增加梯形减速，尚未测试

--- application/cmd/robot_cmd.c
+++ application/cmd/robot_cmd.c
@@ -14,6 +14,7 @@
 #include "bsp_dwt.h"
 #include "bsp_log.h"
 #include "bsp_usb.h"
+#include "cmsis_os.h"
 // 私有宏,自动将编码器转换成角度值
 #define YAW_ALIGN_ANGLE (YAW_CHASSIS_ALIGN_ECD * ECD_ANGLE_COEF_DJI) // 对齐时的角度,0-360
 #define PTICH_HORIZON_ANGLE (PITCH_HORIZON_ECD * ECD_ANGLE_COEF_DJI) // pitch水平时电机的角度,0-360
@@ -33,7 +34,7 @@ static Robot_Status_e Robot_State;//机器人整体工作状态
 static attitude_t *IMU_data;
 
 static float Target_Yaw_Angele = 0, Target_Yaw_Angular_Velocity = 0, Yaw_Offset = 0;
-
+static   float trapezoid_Velocity_max=0,last_velocity=0;//梯形加减速
 void RobotCMDInit()
 {
 
@@ -92,7 +93,20 @@ static void RemoteControlSet(void)
     {
         Chassis_Cmd_Send.target_yaw_angle += 360.0f;
     }
-    // Target_Yaw_Angele += rc_data[TEMP].rc.rocker_r_ * 0.005f;
+    if (last_velocity>Chassis_Cmd_Send.vx)
+      {
+        trapezoid_Velocity_max=last_velocity;
+      }
+  else
+    {
+      last_velocity=Chassis_Cmd_Send.vx;
+    }
+
+  if ((abs(rc_data[TEMP].rc.rocker_l1)<10) && (fabs(IMU_data->Accel[0])>10 || fabs(IMU_data->Accel[1])>10 || fabs(IMU_data->Accel[2])>10))
+    {
+          trapezoid_Velocity_max = trapezoid_Velocity_max * 0.9f;
+    }
+  // Target_Yaw_Angele += rc_data[TEMP].rc.rocker_r_ * 0.005f;
     //
     // while (Target_Yaw_Angele - IMU_data->Yaw >= 180.0f)
     // {
